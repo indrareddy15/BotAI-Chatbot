2b6597a928be5415279efb0e03af4ad4f1c54f28
indrareddy15 <gantaindrareddy83@gmail.com>
2024-10-31 10:53:03 +0530

    routing fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import PastConversation from './Pages/PastConversation/PastConversation';
+import HomePage from "./Pages/HomePage/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "pastconversation",
+        element: <PastConversation />
+      },
+      {
+        path: "/",
+        element: <HomePage />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
